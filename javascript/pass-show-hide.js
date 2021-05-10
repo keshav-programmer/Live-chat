cd020fd9fb42f0a3fd42c879840d7d03bf52049d
Keshav <alkaguptasanjay@gmail.com>
2021-05-10 02:45:30 -0400

    Complete sign up page

--- javascript/pass-show-hide.js
+++ javascript/pass-show-hide.js
@@ -10,3 +10,19 @@ toggleBtn.onclick = ()=>{
         toggleBtn.classList.remove("active");
     }
 }
+
+var step1 = document.getElementById("step1");
+var step2 = document.getElementById("step2");
+var step3 = document.getElementById("step3");
+
+var btn1 = document.getElementById("Btn1");
+var btn2= document.getElementById("Btn2");
+
+btn1.onclick = ()=>{
+    step1.style.display = "none";
+    step2.style.display = "block";
+}
+btn2.onclick = ()=>{
+    step2.style.display = "none";
+    step3.style.display = "block";
+}
