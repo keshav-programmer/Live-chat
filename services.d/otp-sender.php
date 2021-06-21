44ca552325a4549f71756e88adc25a63f82354a2
Sazad Ahemad <60960500+connect2sazad@users.noreply.github.com>
2021-06-21 10:47:30 +0530

    updated otp sender

--- services.d/otp-sender.php
+++ services.d/otp-sender.php
@@ -6,13 +6,74 @@
 
     $otp = randNum(6);
 
-    $prepare = "
-        <h2>Hii ".$fname.",<h2>
-        <p>Use the following one-time password (OTP) to verify your email address. You can use this email address to sign-up or recover your Live Chat account.</p>
-        <h2>".$otp."<h2><br>
-        <p>Regards,</p>
-        <p>Live Chat Team</p>
-    ";
+    // $prepare = "
+    //     <h2>Hii ".$fname.",<h2>
+    //     <p>Use the following one-time password (OTP) to verify your email address. You can use this email address to sign-up or recover your Live Chat account.</p>
+    //     <h2>".$otp."<h2><br>
+    //     <p>Regards,</p>
+    //     <p>Live Chat Team</p>
+    // ";
+
+    $prepare = '
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>OTP </title>
+        <style>
+    
+            @import url("https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap");
+    
+             body{
+                 font-family: "Poppins",sans-serif;
+                 background-color: #cc9966;
+             }
+             #name{
+                font-weight: 800;
+                font-family: cursive;
+             }
+             p{
+                 font-size: 20px;
+             }
+             #otp{
+                 border: 2px solid black;
+                 border-radius: 5px;
+                 font-size: 1.5em;
+                 padding-left: 10px;
+                 padding-right: 10px;
+		 padding: 15px;
+             }
+             #Contact{
+                 position: relative;
+                 top: -20px;
+             }
+             #logo{
+                 width: 40px;
+                 height: 40px;
+                 margin-top: 10px;
+                 /* position: relative; */
+                 /* bottom: -55px; */
+                 /* left: -100px; */
+             }
+    
+        </style>
+    </head>
+    <body>
+    
+      <center>  <h1 id="header">Hii <span id="name">'.$fname.'</span>!</h1></center>
+        <p>Use the following one-time password(OTP) to sign-in to your Live-chat Account.</p><br>
+    
+       <center> <span id="otp"><b>'.$otp.'</b></span></center><br>
+       <p>(The OTP will be valid for the next 45 seconds)</p><p>If you didn\'t intiate this action or if you think you received this email by mistake, please contact</p><div id="Contact"><a href="#">xyz@gmail.com</a></div><br>
+      <b> Regards</b>,<br>
+      <img src="https://github.com/keshav-programmer/Live-chat/blob/keshav-patch-2/otp%20mail/logo.png" id="logo"><br>
+       Live-Chat Team. 
+        
+    </body>
+    </html>
+    ';
 
     $subject = "OTP for your Live Chat Account";
 
@@ -23,7 +84,7 @@
     // More headers
     $headers .= 'From: <no-reply@abc.com>' . "\r\n";
 
-    // mail($email, $subject, $prepare, $headers);
+    mail($email, $subject, $prepare, $headers);
     
 
     echo $otp;
