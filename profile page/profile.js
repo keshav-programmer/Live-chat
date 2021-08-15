2a25a642e58e92a1085aa82c96220ccc7dc1d377
Keshav <alkaguptasanjay@gmail.com>
2021-08-15 02:11:58 -0400

    design complete with error

--- profile page/profile.js
+++ profile page/profile.js
@@ -1,32 +1,7 @@
-$(document).ready(function() {
-    if($('#profile-name-input').css('border') == 'none'){
-        $('#arrow-right').css('display', 'none');
-        $('#pencil').click(function(){
+let profileinput = document.querySelector('profile-name-input');
 
-            $('#profile-name-input').css('border', '1px solid black');
+let pencil = document.querySelector('pencil');
 
-        })
-    }else{
-        $('#pencil').css('display','none')
-        $('#arrow-right').click(function(){
-
-            $('#profile-name-input').css('border', 'none')
-
-        })
-    }
-})
-
-
-//  
-// let profileInput = document.getElementsByClassName('.profile-input');
-// let btn = document.getElementById('#pencil');
-
-// if(profileInput.style.border === 'none'){
-//     btn.addEventListener('click',()=>{
-//         profileInput.style.border = 'solid';
-//     })
-// }else{
-//     btn.addEventListener('click',()=>{
-//         profileInput.style.border = 'none';
-//     })
-// }
+pencil.onclick = ()=>{
+    profileinput.style.border = 'dotted 1px black';
+}
